ad3f605bc4082f01c3473259c2793ae11d6ec121
Eduardo Marques <65765455+EduardoRMarks@users.noreply.github.com>
2024-02-29 19:26:21 +0000

    tutorial 1 v6

--- code/app/src/main/java/dam_a45977/helloworld/MainActivity.kt
+++ code/app/src/main/java/dam_a45977/helloworld/MainActivity.kt
@@ -6,25 +6,8 @@ import android.widget.Button
 import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
-
-    private var btn_sh_info: Button? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btn_sh_info = findViewById(R.id.button_info)
-
-        btn_sh_info?.setOnClickListener {
-            val dialog = CustomDialog()
-            dialog.show(supportFragmentManager, "CustomDialog")
-        }
-    }
-
-    class CustomDialog {
-        fun show(supportFragmentManager: FragmentManager, s: String) {
-            TODO("Not yet implemented")
-        }
-
     }
 }
