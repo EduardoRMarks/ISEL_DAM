2261a2d94245820ff020626711d63096b6a76cc2
Eduardo Marques <65765455+EduardoRMarks@users.noreply.github.com>
2024-03-02 17:20:44 +0000

    hello world optional

---
+++ tutorial1_a45977/helloWorld/app/src/main/java/dam_a45977/helloworld/MainActivity.kt
@@ -0,0 +1,13 @@
+package dam_a45977.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
