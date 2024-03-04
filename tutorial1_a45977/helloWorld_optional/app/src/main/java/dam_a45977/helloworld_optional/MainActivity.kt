6389de1fa1a0350e4fbf46ef9d6bb5d18df6b0f7
Eduardo Marques <65765455+EduardoRMarks@users.noreply.github.com>
2024-03-04 18:22:19 +0000

    hello world optional v4

--- tutorial1_a45977/helloWorld_optional/app/src/main/java/dam_a45977/helloworld_optional/MainActivity.kt
+++ tutorial1_a45977/helloWorld_optional/app/src/main/java/dam_a45977/helloworld_optional/MainActivity.kt
@@ -3,6 +3,7 @@ package dam_a45977.helloworld_optional
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val deviceInfo: TextView = findViewById(R.id.model_info)
+        deviceInfo.inputType = InputType.TYPE_NULL
+        deviceInfo.isSingleLine = false
 
         val deviceInfoAux = "Manufacturer: ${Build.MANUFACTURER}\n" +
                 "Model: ${Build.MODEL}\n" +
@@ -24,5 +27,6 @@ class MainActivity : AppCompatActivity() {
                 "Display: ${Build.DISPLAY}\n"
 
         deviceInfo.text = deviceInfoAux
+
     }
 }
