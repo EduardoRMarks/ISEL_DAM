b442017b962025b16b4c80aa84396dc827db0ece
Eduardo Marques <65765455+EduardoRMarks@users.noreply.github.com>
2024-03-04 17:53:24 +0000

    hello world optional v3

--- tutorial1_a45977/helloWorld_optional/app/src/main/java/dam_a45977/helloworld_optional/MainActivity.kt
+++ tutorial1_a45977/helloWorld_optional/app/src/main/java/dam_a45977/helloworld_optional/MainActivity.kt
@@ -4,17 +4,13 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.annotation.RequiresApi
-import androidx.core.text.bold
-import dam_a45977.helloworld_optional.R.*
 
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        val deviceInfo: TextView = findViewById(id.model_info)
+        val deviceInfo: TextView = findViewById(R.id.model_info)
 
         val deviceInfoAux = "Manufacturer: ${Build.MANUFACTURER}\n" +
                 "Model: ${Build.MODEL}\n" +
