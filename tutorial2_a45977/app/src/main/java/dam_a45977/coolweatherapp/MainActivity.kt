0d0ef20d73de3b6ed75e0b1ee0ab25073fc3e986
Eduardo Marques <65765455+EduardoRMarks@users.noreply.github.com>
2024-03-07 22:42:37 +0000

    tutorial 2 v2

--- tutorial2_a45977/app/src/main/java/dam_a45977/coolweatherapp/MainActivity.kt
+++ tutorial2_a45977/app/src/main/java/dam_a45977/coolweatherapp/MainActivity.kt
@@ -1,10 +1,21 @@
 package dam_a45977.coolweatherapp
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val day = false
+        when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT ->
+                if(day) setTheme(R.style.Theme_Day)
+                else setTheme(R.style.Theme_Night)
+            Configuration.ORIENTATION_LANDSCAPE ->
+                if(day) setTheme(R.style.Theme_Day_Land)
+                else setTheme(R.style.Theme_Night_Land)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
