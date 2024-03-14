1151a5403746dc23b86f75a35be6171d263a7343
Eduardo Marques <65765455+EduardoRMarks@users.noreply.github.com>
2024-03-14 23:59:14 +0000

    tutorial 2 v8

--- tutorial2_a45977/app/src/main/java/dam_a45977/coolweatherapp/MainActivity.kt
+++ tutorial2_a45977/app/src/main/java/dam_a45977/coolweatherapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.res.Configuration
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
@@ -13,13 +14,21 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import com.google.gson.Gson
+import java.io.Console
+import java.io.InputStreamReader
+import java.lang.Math.round
+import java.net.URL
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
-    private var latitude:Double = 38.74
+    private var latitude:Double = 39.74
     private var longitude:Double = -9.15
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        fetchWeatherData(latitude.toFloat(), longitude.toFloat()).start()
 
         val currentNightMode = resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK
         val day = currentNightMode == android.content.res.Configuration.UI_MODE_NIGHT_NO
@@ -34,6 +43,11 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val latitudetext: TextView = findViewById(R.id.latitude)
+        val longitudetext: TextView = findViewById(R.id.longitude)
+        latitudetext.text = latitude.toString()
+        longitudetext.text = longitude.toString()
 
         setDayNightTheme(day)
         setStatusBarColor(day)
@@ -95,4 +109,53 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-}
+
+    private fun WeatherAPI_Call(lat: Float, long: Float) : WeatherData {
+        val reqString = buildString {
+            append("https://api.open-meteo.com/v1/forecast?")
+            append("latitude=${lat}&longitude=${long}&")
+            //append("current_weather=true&")
+            append("current=temperature_2m,relative_humidity_2m,apparent_temperature," +
+                    "is_day,precipitation,weather_code,pressure_msl,wind_speed_10m," +
+                    "wind_direction_10m,wind_gusts_10m&daily=temperature_2m_max," +
+                    "temperature_2m_min,uv_index_max&timezone=auto")
+    }
+        val url = URL(reqString)
+        url.openStream().use {
+            return Gson().fromJson(InputStreamReader(it,"UTF-8"),WeatherData::class.java)
+        }
+    }
+
+    private fun fetchWeatherData(lat: Float, long: Float) : Thread {
+        return Thread {
+            val weather = WeatherAPI_Call(lat,long)
+            updateUI(weather)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateUI(request: WeatherData) {
+        runOnUiThread() {
+            val mapt = getWeatherCodeMap()
+
+            val time: TextView = findViewById(R.id.time_value)
+            val min_max: TextView = findViewById(R.id.min_max)
+            val temperature: TextView = findViewById(R.id.temp)
+            val thermalSensation: TextView = findViewById(R.id.thermal_sensation)
+            val wind: TextView = findViewById(R.id.wind_value)
+            val wind_gust: TextView = findViewById(R.id.wind_gust_value)
+            val humidity: TextView = findViewById(R.id.humidity_value)
+            val pressure: TextView = findViewById(R.id.pressure_value)
+
+            time.text = request.current.time.substring(11)
+            min_max.text = "(" + round(request.daily.temperature_2m_min[0]).toString() + "º/" + round(request.daily.temperature_2m_max[0]).toString() + "º)"
+            temperature.text = round(request.current.temperature_2m).toString() + "º"
+            thermalSensation.text = round(request.current.apparent_temperature).toString() + "º"
+            wind.text = request.current.wind_speed_10m.toString()
+            wind_gust.text = request.current.wind_gusts_10m.toString()
+            humidity.text = request.current.relative_humidity_2m.toString() + "%"
+            pressure.text = request.current.pressure_msl.toString() + "hPa"
+            Log.d("aaaaaaaaaaaaaa", request.current.weathercode.toString())
+        }
+    }
+    }
