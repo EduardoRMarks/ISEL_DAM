be94431dc58f412d4213d756db41a70dd8ff63af
Eduardo Marques <65765455+EduardoRMarks@users.noreply.github.com>
2024-03-16 16:41:33 +0000

    tutorial 2 v12

--- tutorial2_a45977/app/src/main/java/dam_a45977/coolweatherapp/MainActivity.kt
+++ tutorial2_a45977/app/src/main/java/dam_a45977/coolweatherapp/MainActivity.kt
@@ -220,7 +220,7 @@ class MainActivity : AppCompatActivity() {
 
     fun degreesToDirection(degrees: Float, day: Boolean): String {
         val directions = arrayOf("s", "sw", "w", "nw", "n", "ne", "e", "se", "s")
-        val index = (((degrees % 360 + 360) +22.5) % 360 / 45).toInt()
+        val index = ((((degrees % 360 + 360)) % 360 + 22.5) / 45).toInt()
         return if (day) directions[index] + "_day"
         else directions[index] + "_night"
     }
